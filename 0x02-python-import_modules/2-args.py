fe831fefe2c3ea2bc23713e33367e107b19b98f2
Wiston Venera Macías <wisvem@hotmail.com>
2020-12-10 19:32:54 +0000

    Task 2

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -2,8 +2,10 @@
 if __name__ == "__main__":
     import sys
 i = 0
-print("{} Arguments".format(len(sys.argv) - 1), end="")
-print(".") if len(sys.argv) < 2 else print(":")
+if len(sys.argv) < 2:
+    print("{} Arguments.".format(len(sys.argv) - 1))
+else:
+    print("{} Arguments:".format(len(sys.argv) - 1))
 for arg in sys.argv:
     if (i > 0):
         print("{}: {}".format(i, arg))
