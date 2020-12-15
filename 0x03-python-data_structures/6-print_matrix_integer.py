0f6936be44c8cde4526a54007a818ace6ba91b8e
Wiston Venera Macías <wisvem@hotmail.com>
2020-12-15 21:59:31 +0000

    Task 6

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
-    for r in matrix:
-        for c in range(len(r)):
-            print("{}".format(r[c]), end="\n" if len(r)-1 == c else " ")
+    if matrix:
+        for r in matrix:
+            for c in range(len(r)):
+                print("{}".format(r[c]), end="\n" if len(r)-1 == c else " ")
