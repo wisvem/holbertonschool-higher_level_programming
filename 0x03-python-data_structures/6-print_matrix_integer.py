82c7a23b746e2e5ecbc1a8afe30a77c583506847
Wiston Venera Macías <wisvem@hotmail.com>
2020-12-15 22:08:50 +0000

    Task 6 - Hanlded empty matrix

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
-    if matrix:
         for r in matrix:
+            if len(r) == 0:
+                print()
             for c in range(len(r)):
                 print("{:d}".format(r[c]), end="\n" if len(r)-1 == c else " ")
