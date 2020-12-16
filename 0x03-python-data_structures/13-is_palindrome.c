c7675b1ca8c891480edf2640bb70aca67baa6a95
Wiston Venera Macías <wisvem@hotmail.com>
2020-12-16 05:37:59 +0000

    Task 13

--- 0x03-python-data_structures/13-is_palindrome.c
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -1,31 +1,6 @@
 #include "lists.h"
 
 /**
-* palin - check array
-* @arr: array to check
-* @n: size
-* Return: 0 si, 1 no
-**/
-int palin(int arr[], int n)
-{
-	int i, j = n - 1, flag = 0;
-
-	for (i = 0; i < n / 2; i++)
-	{
-		if (arr[i] != arr[j])
-		{
-			flag = 1;
-			break;
-		}
-		j--;
-	}
-	if (flag == 1)
-		return (0);
-	else
-		return (1);
-}
-
-/**
 * is_palindrome - checks if a list is palindrome
 * @head: pointer to the list
 * Return: 0 if it is not a palindrome, 1 if it is a palindrome
@@ -33,20 +8,23 @@ int palin(int arr[], int n)
 int is_palindrome(listint_t **head)
 {
 	listint_t *start;
-	int i = 0;
+	int i = 0, j;
 	int t[1024];
 
-	if (*head != NULL)
+	start = *head;
+	i = 0;
+	while (start != NULL)
 	{
-		start = *head;
-		i = 0;
-		while (start != NULL)
+		t[i] = (*start).n;
+		start = (*start).next;
+		i++;
+	}
+	for (j = 0; j < (i / 2); j++)
+	{
+		if (t[i - j - 1] != t[j])
 		{
-			t[i] = (*start).n;
-			start = (*start).next;
-			i++;
+			return (0);
 		}
-		return (palin(t, i));
 	}
 	return (1);
 }
