c0e11ed88b07cba5cee80ca7572c9f59865775f6
Wiston Venera Macías <wisvem@hotmail.com>
2020-12-16 05:58:22 +0000

    Task 13

--- 0x03-python-data_structures/13-is_palindrome.c
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -1,30 +1,6 @@
 #include "lists.h"
 
 /**
-* palin - palindrome functio
-* @s: String to evaluate
-* @i: an auxiliar
-* @lenght: length of @s
-* Return: 1 if string is a palindrome and 0 if not.
-**/
-int palin(int *s, int i, int lenght)
-{
-	if (i == lenght)
-	{
-		return (1);
-	}
-	if (s[i] != s[lenght])
-	{
-		return (0);
-	}
-	if (i < lenght)
-	{
-		return (palin(s, i + 1, lenght - 1));
-	}
-	return (1);
-}
-
-/**
 * is_palindrome - checks if a list is palindrome
 * @head: pointer to the list
 * Return: 0 if it is not a palindrome, 1 if it is a palindrome
@@ -32,7 +8,7 @@ int palin(int *s, int i, int lenght)
 int is_palindrome(listint_t **head)
 {
 	listint_t *start = *head;
-	int i = 0, t[1024];
+	int i = 0, j, t[1024];
 
 	while (start != NULL)
 	{
@@ -40,6 +16,12 @@ int is_palindrome(listint_t **head)
 		start = (*start).next;
 		i++;
 	}
-	return (palin(t, 0, i - 1));
+	for (j = 0; j < i; j++)
+	{
+		if (t[j] != t[i - j - 1])
+		{
+			return (0);
+		}
+	}
 	return (1);
 }
