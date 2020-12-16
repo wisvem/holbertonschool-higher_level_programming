e5f39f5ab67e6c9941e6ba34ad291afc778802ed
Wiston Venera Macías <wisvem@hotmail.com>
2020-12-16 04:40:17 +0000

    Task 13

--- 0x03-python-data_structures/13-is_palindrome.c
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -1,4 +1,37 @@
 #include "lists.h"
+/**
+ * palindrome_check - check
+ * @palindrome: list
+ * @counter: size
+ * Return: 1 and 0
+ */
+int palindrome_check(listint_t *palindrome, int counter)
+{
+	int i = 0, j;
+	listint_t *front, *rear;
+
+	while (i != counter / 2)
+	{
+		front = rear = palindrome;
+		for (j = 0; j < i; j++)
+		{
+			front = front->next;
+		}
+		for (j = 0; j < counter - (i + 1); j++)
+		{
+			rear = rear->next;
+		}
+		if (front->n != rear->n)
+		{
+			return (0);
+		}
+		else
+		{
+			i++;
+		}
+	}
+	return (1);
+}
 
 /**
 * listint_len - Entry point
@@ -22,31 +55,6 @@ int listint_len(const listint_t *h)
 	return (elements);
 }
 
-/**
-* palin - check array
-* @arr: array to check
-* @n: size
-* Return: 0 si, 1 no
-**/
-int palin(int arr[], int n)
-{
-	int i, j = n - 1, flag = 0;
-
-	for (i = 0; i < n / 2; i++)
-	{
-		if (arr[i] != arr[j])
-		{
-			flag = 1;
-			break;
-		}
-		j--;
-	}
-	free(arr);
-	if (flag == 1)
-		return (0);
-	else
-		return (1);
-}
 
 /**
 * is_palindrome - checks if a list is palindrome
@@ -55,19 +63,24 @@ int palin(int arr[], int n)
 **/
 int is_palindrome(listint_t **head)
 {
-	listint_t *start;
-	int i = 0;
-	int *t = NULL;
-
-	start = *head;
-	i = listint_len(*head);
-	t = malloc(sizeof(int) * i);
-	i = 0;
-	while (start != NULL)
-	{
-		t[i] = (*start).n;
-		start = (*start).next;
-		i++;
-	}
-	return (palin(t, i));
+	listint_t *temp = *head;
+	int i = listint_len(temp);
+/*
+*	listint_t *start;
+*	int i = 0;
+*	int *t = NULL;
+*
+*	start = *head;
+*	i = listint_len(*head);
+*	t = malloc(sizeof(int) * i);
+*	i = 0;
+*	while (start != NULL)
+*	{
+*		t[i] = (*start).n;
+*		start = (*start).next;
+*		i++;
+*	}
+*	return (palin(t, i));
+*/
+	return (palindrome_check(*head, i));
 }
