571f0c435a0e8ac857d021c0ec2f31bb939f3943
Wiston Venera Macías <wisvem@hotmail.com>
2020-12-16 05:13:56 +0000

    Task 14

--- 0x03-python-data_structures/100-print_python_list_info.c
+++ 0x03-python-data_structures/100-print_python_list_info.c
@@ -7,13 +7,11 @@
 void print_python_list_info(PyObject *p)
 {
 	int i;
-	PyObject *x;
 
 	printf("[*] Size of the Python List = %d\n", ((int)Py_SIZE(p)));
 	printf("[*] Allocated = %d\n", ((int)((PyListObject *)p)->allocated));
 	for (i = 0; i < Py_SIZE(p); i++)
 	{
-		x = ;
 		printf("Element %d: %s\n", i, Py_TYPE(PyList_GetItem(p, i))->tp_name);
 	}
 }
