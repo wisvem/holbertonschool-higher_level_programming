9956eb8231b69a39d6051b864e566012b3936067
Wiston Venera Macías <wisvem@hotmail.com>
2020-12-17 20:08:41 +0000

    task 10

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 def best_score(a_dictionary):
-    if(a_dictionary):
-        for key in sorted(a_dictionary.keys(), reverse=True):
-            return key
+    for key in sorted(a_dictionary.keys(), reverse=True):
+        return key
+    
