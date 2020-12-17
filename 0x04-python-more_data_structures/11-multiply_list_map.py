aafba0be3167e373f66bdab85fb8078cbc751944
Wiston Venera Macías <wisvem@hotmail.com>
2020-12-17 22:09:44 +0000

    Task 11

--- 0x04-python-more_data_structures/11-multiply_list_map.py
+++ 0x04-python-more_data_structures/11-multiply_list_map.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 def multiply_list_map(my_list=[], number=0):
     if my_list:
-        return set(map(lambda x: x*number, my_list))
+        return list(map(lambda x: x*number, my_list))
