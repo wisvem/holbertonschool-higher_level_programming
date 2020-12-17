5a7fc6a607a42f20be89cbce41d3741c090b1f45
Wiston Venera Macías <wisvem@hotmail.com>
2020-12-17 17:20:51 +0000

    Task 2 - handled empty list

--- 0x04-python-more_data_structures/2-uniq_add.py
+++ 0x04-python-more_data_structures/2-uniq_add.py
@@ -1,8 +1,10 @@
 #!/usr/bin/python3
 def uniq_add(my_list=[]):
-    my_list.sort()
-    res = my_list[0]
-    for i in range(len(my_list)-1):
-        if (my_list[i] != my_list[i+1]):
-            res = res + my_list[i+1]
+    res = 0
+    if my_list:
+        my_list.sort()
+        res = my_list[0]
+        for i in range(len(my_list)-1):
+            if (my_list[i] != my_list[i+1]):
+                res = res + my_list[i+1]
     return res
