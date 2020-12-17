1ff9c5807f98fcfb492c001efecd4db94ce2bf71
Wiston Venera Macías <wisvem@hotmail.com>
2020-12-17 17:15:48 +0000

    Task 0 - 3

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,8 +1,7 @@
 #!/usr/bin/python3
 def square_matrix_simple(matrix=[]):
-    square = lambda x : x * x
     matrix2 = list(map(list, matrix))
     for r in matrix2:
         for c in range(len(r)):
-            r[c] = square(r[c])
+            r[c] = r[c]*r[c]
     return matrix2
