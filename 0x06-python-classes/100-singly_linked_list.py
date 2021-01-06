2b579048f9f4deb970d3b170f2ce69ea0563001d
Wiston Venera Macías <wisvem@hotmail.com>
2021-01-06 20:54:24 +0000

    Task 7 fix

--- 0x06-python-classes/100-singly_linked_list.py
+++ 0x06-python-classes/100-singly_linked_list.py
@@ -6,76 +6,35 @@ class Node:
     '''class to create Nodes'''
 
     def __init__(self, data, next_node=None):
-        """[summary]
-
-        Args:
-            data ([type]): [description]
-            next_node ([type], optional): [description]. Defaults to None.
-        """
         self.data = data
         self.next_node = next_node
 
     @property
     def data(self):
-        """[summary]
-
-        Returns:
-            [type]: [description]
-        """
         return self.__data
 
     @property
     def next_node(self):
-        """[summary]
-
-        Returns:
-            [type]: [description]
-        """
         return self.__next_node
 
     @data.setter
     def data(self, value):
-        """[summary]
-
-        Args:
-            value ([type]): [description]
-
-        Raises:
-            TypeError: [description]
-        """
         if type(value) is not int:
             raise TypeError("data must be an integer")
         self.__data = value
 
     @next_node.setter
     def next_node(self, value):
-        """[summary]
-
-        Args:
-            value ([type]): [description]
-
-        Raises:
-            TypeError: [description]
-        """
         if type(value) is not Node and value is not None:
             raise TypeError("next_node must be a Node object")
         self.__next_node = value
 
 
 class SinglyLinkedList:
-    """Class to create single linked list"""
-
     def __init__(self):
-        """[summary]
-        """
         self.__head = None
 
     def sorted_insert(self, value):
-        """[summary]
-
-        Args:
-            value ([type]): [description]
-        """
         curr = self.__head
         newNode = Node(value)
         if not self.__head:
@@ -94,11 +53,6 @@ class SinglyLinkedList:
         return
 
     def __str__(self):
-        """[summary]
-
-        Returns:
-            [type]: [description]
-        """
         list = ""
         if self.__head is None:
             return list
