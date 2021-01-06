3e48d2a0c84d0fdbc5266acd189b9af6383cd93d
Wiston Venera Macías <wisvem@hotmail.com>
2021-01-06 21:21:39 +0000

    task 7 changed f-string to str()

--- 0x06-python-classes/100-singly_linked_list.py
+++ 0x06-python-classes/100-singly_linked_list.py
@@ -1,80 +1,43 @@
 #!/usr/bin/python3
-"""Linked List Module"""
+'''Single Linked list module'''
 
 
 class Node:
-    """Class to manipulate Nodes and no more"""
+    '''Node class'''
 
     def __init__(self, data, next_node=None):
-        """Method to initialize
-
-        Args:
-            data ([int]): [integer]
-            next_node ([Node], optional): [description]. Defaults to None.
-        """
         self.data = data
         self.next_node = next_node
 
     @property
     def data(self):
-        """Data getter
-
-        Returns:
-            [int]: [integer]
-        """
         return self.__data
 
     @data.setter
     def data(self, value):
-        """Method to set data
-
-        Args:
-            value ([type]): [description]
-
-        Raises:
-            TypeError: [description]
-        """
         if type(value) is not int:
             raise TypeError("data must be an integer")
         self.__data = value
 
     @property
     def next_node(self):
-        """Getter for next_node
-
-        Returns:
-            [Node]: [description]
-        """
         return self.__next_node
 
     @next_node.setter
     def next_node(self, value):
-        """Method to set node
 
-        Args:
-            value ([type]): [description]
-
-        Raises:
-            TypeError: [description]
-        """
         if type(value) is not Node and value is not None:
             raise TypeError("next_node must be a Node object")
         self.__next_node = value
 
 
 class SinglyLinkedList:
-    """Single linked list class"""
+    '''Single linked list class'''
 
     def __init__(self):
-        """Method for initialize a Link object"""
         self.__head = None
 
     def sorted_insert(self, value):
-        """Method to add node
-
-        Args:
-            value ([type]): [description]
-        """
         curr = self.__head
         newNode = Node(value)
         if not self.__head:
@@ -93,18 +56,13 @@ class SinglyLinkedList:
         return
 
     def __str__(self):
-        """[Overloading str]
-
-        Returns:
-            [type]: [description]
-        """
         listx = ""
         if self.__head is None:
             return listx
         curr = self.__head
         while curr.next_node is not None:
-            listx += f'{curr.data}'
+            listx += str(curr.data)
             listx += "\n"
             curr = curr.next_node
-        listx += f'{curr.data}'
+        listx += str(curr.data)
         return listx
