66ff9a79b229f3df5da7ed7af857e9007bfe8f22
Wiston Venera Macías <wisvem@hotmail.com>
2021-01-08 20:05:52 -0500

    Task 4 and test

---
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -0,0 +1,54 @@
+#!/usr/bin/python3
+"""Indent text module"""
+
+
+def text_indentation(text):
+    """Function that prints a text with
+    2 new lines after each of these characters: ., ?
+
+    Args:
+        text (str): text to indent
+
+    Raises:
+        TypeError: if no str type
+    """
+    newText = ""
+    sentence = ""
+    if type(text) is not str:
+        raise TypeError("text must be a string")
+    pos = 0
+    while pos in range(len(text)):
+        letter = text[pos]
+        if letter not in (".", "?", ":"):
+            sentence += letter
+        else:
+            try:
+                while sentence[0] == " ":
+                    sentence = sentence[1:]
+            except IndexError:
+                pass
+            try:
+                while sentence[len(sentence)-1] == " ":
+                    sentence = sentence[:-1]
+            except IndexError:
+                pass
+            newText = newText + sentence + letter + "\n\n"
+            sentence = ""
+        pos = pos+1
+    try:
+        while sentence[0] == " ":
+            sentence = sentence[1:]
+    except IndexError:
+        pass
+    try:
+        while sentence[len(sentence)-1] == " ":
+            sentence = sentence[:-1]
+    except IndexError:
+        pass
+    newText = newText + sentence
+    print(newText, end="")
+
+
+if __name__ == "__main__":
+    import doctest
+    doctest.testfile("tests/4-print_square.txt")
