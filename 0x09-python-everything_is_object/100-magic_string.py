12220de5a0203bdfe17ea04d2df7eef33f0c5021
Wiston Venera Macías <wisvem@hotmail.com>
2021-01-12 19:02:58 +0000

    Remain tasks

--- 0x09-python-everything_is_object/100-magic_string.py
+++ 0x09-python-everything_is_object/100-magic_string.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
 def magic_string():
     count = 0
-    setattr(magic_string, "count", count+1)
-    print(count)
-    return "Holberton"
+
+    return "Holberton";  setattr(magic_string, "count", count+1)
