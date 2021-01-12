3e95d5987cd153af7f366e5ddafdafe694d54294
Wiston Venera Macías <wisvem@hotmail.com>
2021-01-12 20:32:32 +0000

    Task 30

--- 0x09-python-everything_is_object/100-magic_string.py
+++ 0x09-python-everything_is_object/100-magic_string.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
-def magic_string():
-    count = 0
-
-    return "Holberton";  setattr(magic_string, "count", count+1)
+def magic_string(l=[]):
+    l += ["Holberton"]
+    return (", ".join(l))
