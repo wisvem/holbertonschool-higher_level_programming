ae635cb625b6e7aa0080d608ff38c133280f418f
Wiston Venera Macías <wisvem@hotmail.com>
2021-01-19 18:09:03 -0500

    pep8

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -1,8 +1,20 @@
 #!/usr/bin/python3
+"""[summary]
+    """
+
+
 class MyList(list):
+    """[summary]
+
+    Args:
+        list ([type]): [description]
+    """
+
     def __init__(self):
         pass
 
     def print_sorted(self):
+        """[summary]
+        """
         nl = self.copy()
         print(sorted(nl))
