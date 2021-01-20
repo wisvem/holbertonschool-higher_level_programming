558172996c8c522d17c2d6303bc82e7fdc660bc6
Wiston Venera Macías <wisvem@hotmail.com>
2021-01-20 18:55:49 -0500

    Task 7 handled error

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -12,6 +12,6 @@ sys.argv.pop(0)
 my_list = sys.argv
 try:
     my_list = [*loadjson(filename), *my_list]
-except:
-    FileNotFoundError
+except FileNotFoundError:
+    pass
 savejson(my_list, filename)
