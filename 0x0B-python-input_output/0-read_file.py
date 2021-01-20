067af750ddab3d46863700e5beb5d75c2136b180
Wiston Venera Macías <wisvem@hotmail.com>
2021-01-20 17:41:30 -0500

    Task 0

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -8,5 +8,5 @@ def read_file(filename=""):
     Args:
         filename (str, optional): [description]. Defaults to "".
     """
-    with open(filename, "r", encoding="utf-8") as f:
+    with open(filename, "r", encoding='utf-8') as f:
         print(f.read())
