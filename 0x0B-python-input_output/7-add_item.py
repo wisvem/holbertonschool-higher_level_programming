209bd4e4bbf148eb310b5e4f4c6bc5d6ce1c9e47
Wiston Venera Macías <wisvem@hotmail.com>
2021-01-20 19:43:59 -0500

    Task 7 another approach

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -5,10 +5,11 @@ import sys
 load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
 filename = "add_item.json"
+
 sys.argv.pop(0)
 my_list = sys.argv
 try:
-    my_list = [*load_from_json_file(filename), *my_list]
+    my_list = load_from_json_file(filename) + my_list
 except Exception:
     pass
 save_to_json_file(my_list, filename)
