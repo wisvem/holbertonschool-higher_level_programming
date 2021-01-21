fb6e08eabea7873e2f11c9b84817b9e7f09e52ef
Wiston Venera Macías <wisvem@hotmail.com>
2021-01-20 21:44:11 -0500

    Task 11

--- 0x0B-python-input_output/11-student.py
+++ 0x0B-python-input_output/11-student.py
@@ -1,30 +1,45 @@
 #!/usr/bin/python3
 """Student module"""
-import json
+
 
 class Student:
     """Student Class"""
 
     def __init__(self, first_name, last_name, age):
-        """INit function"""
+        """init function
+
+        Args:
+            first_name ([type]): [description]
+            last_name ([type]): [description]
+            age ([type]): [description]
+        """
         self.first_name = first_name
         self.last_name = last_name
         self.age = age
 
     def to_json(self, attrs=None):
-        """to json function"""
+        """to json
+
+        Args:
+            attrs ([type], optional): [description]. Defaults to None.
+
+        Returns:
+            [type]: [description]
+        """
         obj = {}
         if attrs is not None:
-            if type(attrs) is list:
-                if (all(isinstance(x, str) for x in attrs)):
-                    for i in attrs:
-                        if hasattr(self, i):
-                            obj[i] = getattr(self, i)
-                    return obj
+            for i in attrs:
+                if hasattr(self, i):
+                    obj[i] = getattr(self, i)
+            return obj
         else:
             return self.__dict__
 
     def reload_from_json(self, json):
-        """Function to reload att"""
+        """reload arguments
+
+        Args:
+            json ([type]): [description]
+        """
         for key, value in json.items():
             setattr(self, key, value)
