399a7e5c5e193bb39d7d5a425171f1beff058c64
Wiston Venera Macías <wisvem@hotmail.com>
2021-01-20 21:03:19 -0500

    Task 10 done

---
+++ 0x0B-python-input_output/10-student.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""Student module"""
+
+
+class Student:
+    """Student Class"""
+
+    def __init__(self, first_name, last_name, age):
+        """INit function"""
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """to json function"""
+        obj = {}
+        if attrs is not None:
+            for i in attrs:
+                if hasattr(self, i):
+                    obj[i] = getattr(self, i)
+            return obj
+        else:
+            return self.__dict__
