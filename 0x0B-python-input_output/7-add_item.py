e4f1766bad277833e6f4b7ba53b056569bb9bd3e
Wiston Venera Macías <wisvem@hotmail.com>
2021-01-20 19:04:00 -0500

    Task 7

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -2,10 +2,8 @@
 """add item module"""
 import sys
 
-
 loadjson = __import__('6-load_from_json_file').load_from_json_file
 savejson = __import__('5-save_to_json_file').save_to_json_file
-
 
 filename = "add_item.json"
 sys.argv.pop(0)
