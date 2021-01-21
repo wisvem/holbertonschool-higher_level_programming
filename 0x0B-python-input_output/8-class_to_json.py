c85d692aab40959eb00af26c901f24739a7371a3
Wiston Venera Macías <wisvem@hotmail.com>
2021-01-20 20:04:13 -0500

    Task 8

---
+++ 0x0B-python-input_output/8-class_to_json.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""class to json module"""
+
+
+def class_to_json(obj):
+    """class to json module
+
+    Args:
+        obj ([type]): [description]
+
+    Returns:
+        [type]: [description]
+    """
+    if hasattr(obj, "__dict__"):
+        return obj.__dict__
+    else:
+        return {}
