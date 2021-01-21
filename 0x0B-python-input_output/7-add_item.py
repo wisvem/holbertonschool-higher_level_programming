06f173c99eb4b727b099e52286f8940ed357b159
Wiston Venera Macías <wisvem@hotmail.com>
2021-01-20 19:15:37 -0500

    Task 7 fix?

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,10 +1,9 @@
 #!/usr/bin/python3
-"""add item module"""
+"""Script to add item to a json file"""
 import sys
 
 loadjson = __import__('6-load_from_json_file').load_from_json_file
 savejson = __import__('5-save_to_json_file').save_to_json_file
-
 filename = "add_item.json"
 sys.argv.pop(0)
 my_list = sys.argv
