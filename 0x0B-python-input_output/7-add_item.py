6f372a11435ebb701767b4dfdd47c8cfe39cd0ec
Wiston Venera Macías <wisvem@hotmail.com>
2021-01-20 19:17:26 -0500

    Task 7 function names changed

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -2,13 +2,13 @@
 """Script to add item to a json file"""
 import sys
 
-loadjson = __import__('6-load_from_json_file').load_from_json_file
-savejson = __import__('5-save_to_json_file').save_to_json_file
+load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
+save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
 filename = "add_item.json"
 sys.argv.pop(0)
 my_list = sys.argv
 try:
-    my_list = [*loadjson(filename), *my_list]
+    my_list = [*load_from_json_file(filename), *my_list]
 except Exception:
     pass
-savejson(my_list, filename)
+save_to_json_file(my_list, filename)
