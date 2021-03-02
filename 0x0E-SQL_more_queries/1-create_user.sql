6d096404aa59dc7a08bf954fd6f97ce4b22fffc4
Wiston Venera Macías <wisvem@hotmail.com>
2021-03-02 17:58:02 +0000

    Task 1

--- 0x0E-SQL_more_queries/1-create_user.sql
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -1,2 +1,2 @@
--- Root user
-CREATE USER 'user_0d_1'@'localhost' 
+-- Root: creates the MySQL server user user_0d_1
+GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
