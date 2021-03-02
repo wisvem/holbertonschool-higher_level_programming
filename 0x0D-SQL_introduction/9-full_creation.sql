e5065d0036c2d01f4bcf1dbb47e4675b614ef5f6
Wiston Venera Macías <wisvem@hotmail.com>
2021-03-02 01:12:21 +0000

    Task 9

--- 0x0D-SQL_introduction/9-full_creation.sql
+++ 0x0D-SQL_introduction/9-full_creation.sql
@@ -1,7 +1,8 @@
 -- Full creation
 CREATE TABLE IF NOT EXISTS second_table(id INT, name VARCHAR(256), score INT);
 INSERT INTO second_table(id, name, score)
-VALUES(1, 'John', 10),
-VALUES(2, 'Alex', 3),
-VALUES(3, 'Bob', 14),
-VALUES(4, 'George', 8);
+VALUES
+(1, "John", 10),
+(2, "Alex", 3),
+(3, "Bob", 14),
+(4, "George", 8);
