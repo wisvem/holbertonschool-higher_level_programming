fbd7f6add8479b6b23aeade83ba30b376c2d8fac
Wiston Venera Macías <wisvem@hotmail.com>
2021-03-03 04:52:41 +0000

    Task 20

--- 0x0E-SQL_more_queries/103-rating_genres.sql
+++ 0x0E-SQL_more_queries/103-rating_genres.sql
@@ -1,8 +1,8 @@
 -- script that lists all genres in the database hbtn_0d_tvshows_rate by their rating
-SELECT SUM(tv_show_ratings.rate) AS rating, tv_genres.name
+SELECT tv_genres.name ,SUM(tv_show_ratings.rate) AS rating
 FROM tv_shows, tv_show_ratings, tv_genres, tv_show_genres
 WHERE tv_show_ratings.show_id=tv_shows.id
 AND tv_genres.id=tv_show_genres.genre_id
 AND tv_show_genres.show_id=tv_show_ratings.show_id
 GROUP BY tv_genres.name
-ORDER BY rating DESC, tv_genres.name;
+ORDER BY rating DESC, tv_genres.name
