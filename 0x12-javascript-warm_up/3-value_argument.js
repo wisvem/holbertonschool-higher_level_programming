761dec4c3eb578852ff289794c7ade7771a47082
Wiston Venera Macias <2170@holbertonschool.com>
2021-03-22 19:46:21 +0000

    Task 3

--- 0x12-javascript-warm_up/3-value_argument.js
+++ 0x12-javascript-warm_up/3-value_argument.js
@@ -1,7 +1,14 @@
 #!/usr/bin/node
 const args = process.argv;
+if (typeof args[2] !== 'undefined') {
+  console.log(args[2]);
+} else {
+  console.log('No argument');
+}
+/*
 if (args.length >= 3) {
   console.log(args[2]);
 } else {
   console.log('No argument');
 }
+*/
