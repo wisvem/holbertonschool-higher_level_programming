b9911784979336f3a4905d44a1e7a06fe605d828
Wiston Venera Macias <2170@holbertonschool.com>
2021-03-22 20:40:26 +0000

    Task 4 - Is a number

--- 0x12-javascript-warm_up/4-concat.js
+++ 0x12-javascript-warm_up/4-concat.js
@@ -1,3 +1,8 @@
 #!/usr/bin/node
 const args = process.argv;
-console.log(args[2] + ' is ' + args[3]);
+const myNum = Number(args[2]);
+if (isNaN(myNum)) {
+  console.log('Not a number');
+} else {
+  console.log('My number: ' + myNum);
+}
