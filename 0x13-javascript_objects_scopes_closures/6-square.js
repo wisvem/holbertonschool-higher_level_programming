5459a1c9f392eb3b9935a29ea88d25eb11d5dea4
Wiston Venera Macias <2170@holbertonschool.com>
2021-03-23 18:11:01 +0000

    Task 6 - Removed unnecessary code

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -7,16 +7,7 @@ class Square extends Squares {
   }
 
   charPrint (c) {
-    if (c === undefined) {
-      super.print();
-    } else {
-      for (let i = 0; i < this.height; i++) {
-        for (let j = 0; j < this.width; j++) {
-          process.stdout.write(c);
-        }
-        console.log();
-      }
-    }
+    super.print(c);
   }
 }
 
