3ac8a3a82e6bb52ae5233f4b0464cdeb449d18d7
Wiston Venera Macias <2170@holbertonschool.com>
2021-03-23 19:21:14 +0000

    Task 8

--- 0x13-javascript_objects_scopes_closures/8-esrever.js
+++ 0x13-javascript_objects_scopes_closures/8-esrever.js
@@ -1,4 +1,8 @@
 #!/usr/bin/node
 exports.esrever = function (list) {
-  return list.reverse();
+  const myArray = [];
+  for (let index = 0; index < list.length; index++) {
+    myArray[index] = list[list.length - index - 1];
+  }
+  return myArray;
 };
