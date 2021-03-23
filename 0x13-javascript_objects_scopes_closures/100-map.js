48477b87e0916f624cf54d53bed0fafa10310e57
Wiston Venera Macias <2170@holbertonschool.com>
2021-03-23 19:56:10 +0000

    Task 11

--- 0x13-javascript_objects_scopes_closures/100-map.js
+++ 0x13-javascript_objects_scopes_closures/100-map.js
@@ -1,5 +1,5 @@
 #!/usr/bin/node
-const list = require('./100-main').list;
+const list = require('./100-data').list;
 const map1 = list.map(x => x * list.indexOf(x));
 console.log(list);
 console.log(map1);
