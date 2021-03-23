6fce34a368e7987614f6fb953ca61c7798b3790c
Wiston Venera Macias <2170@holbertonschool.com>
2021-03-23 18:04:54 +0000

    Task 6

---
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -0,0 +1,23 @@
+#!/usr/bin/node
+const Squares = require('./5-square');
+
+class Square extends Squares {
+  constructor (size) {
+    super(size, size);
+  }
+
+  charPrint (c) {
+    if (c === undefined) {
+      super.print();
+    } else {
+      for (let i = 0; i < this.height; i++) {
+        for (let j = 0; j < this.width; j++) {
+          process.stdout.write(c);
+        }
+        console.log();
+      }
+    }
+  }
+}
+
+module.exports = Square;
