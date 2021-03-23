fe13b5365ab83c8397bb43e270099e025f4d27fb
Wiston Venera Macias <2170@holbertonschool.com>
2021-03-23 17:50:27 +0000

    Task 5 -  fixed typo

--- 0x13-javascript_objects_scopes_closures/5-square.js
+++ 0x13-javascript_objects_scopes_closures/5-square.js
@@ -1,10 +1,10 @@
 #!/usr/bin/node
 const Rectangle = require('./4-rectangle');
 
-class Sqaure extends Rectangle {
+class Square extends Rectangle {
   constructor (size) {
     super(size, size);
   }
 }
 
-module.exports = Sqaure;
+module.exports = Square;
