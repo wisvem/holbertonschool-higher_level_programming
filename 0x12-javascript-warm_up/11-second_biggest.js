21bed4b04ba6233ffc36546a42004dd1da5880ea
Wiston Venera Macias <2170@holbertonschool.com>
2021-03-23 03:16:27 +0000

    Task 11

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -1,10 +1,9 @@
 #!/usr/bin/node
 const argv = process.argv;
-if (argv.length >= 3) {
-  const argc = argv.length;
-  let n1 = argv[2];
-  for (let index = 2; index < argc; index++) {
-    if (Number(n1) < Number(argv[index])) n1 = argv[index];
-  }
-  console.log(n1);
+let smax = 0;
+if (argv.length >= 4) {
+  const myArray = argv.slice(2, argv.length);
+  myArray.sort();
+  smax = myArray[myArray.length - 2];
 }
+console.log(smax);
