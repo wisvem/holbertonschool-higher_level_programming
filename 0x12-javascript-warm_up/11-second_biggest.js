4a6740533e6ca94d61bef1c6af17badefb8710dd
Wiston Venera Macias <2170@holbertonschool.com>
2021-03-23 03:34:31 +0000

    Task 11 - Another solution

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -1,11 +1,31 @@
 #!/usr/bin/node
+const argv = process.argv;
+
+function secondMax (myArray) {
+  if (myArray.length <= 3) {
+    return (0);
+  }
+
+  myArray = myArray.slice(2).map(n => parseInt(n));
+
+  const max = Math.max(...myArray);
+
+  const arr = myArray.filter(arr => arr < max);
+
+  return (Math.max(...arr));
+}
+
+console.log(secondMax(argv));
+
+/*
 const args = process.argv;
 let smax = 0;
 if (args.length >= 4) {
-  let myArray = args.slice(2, args.length);
-  myArray.sort();
-  smax = myArray[myArray.length - 1];
-  myArray = myArray.filter(myArray => myArray < smax);
-  smax = myArray[myArray.length - 1];
+  let mymyArray = args.slice(2, args.length);
+  mymyArray.sort();
+  smax = mymyArray[mymyArray.length - 1];
+  mymyArray = mymyArray.filter(mymyArray => mymyArray < smax);
+  smax = mymyArray[mymyArray.length - 1];
 }
 console.log(smax);
+*/
