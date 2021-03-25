5c78dc63f53b4b2bf9893cb5e2b416fba5b538eb
Wiston Venera Macias <2170@holbertonschool.com>
2021-03-25 18:49:52 +0000

    Task 0

---
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""Conn module"""
+import MySQLdb
+from sys import argv
+
+if __name__ == '__main__':
+    myU = argv[1]
+    myP = argv[2]
+    myDB = argv[3]
+    myH = "localhost"
+    db = MySQLdb.connect(host=myH, port=3306, user=myU, passwd=myP, db=myDB)
+    cur = db.cursor()
+    myQ = "SELECT * FROM states ORDER BY id"
+    cur.execute(myQ)
+    result = cur.fetchall()
+    for row in result:
+        print(row)
