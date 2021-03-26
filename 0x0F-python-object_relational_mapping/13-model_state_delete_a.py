7c7fe08b1073fcb8e31913864b881da14d44a517
Wiston Venera Macias <2170@holbertonschool.com>
2021-03-26 03:11:39 +0000

    Task 13

---
+++ 0x0F-python-object_relational_mapping/13-model_state_delete_a.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""First usage of SLQAlchemy"""
+from sys import argv
+from model_state import Base, State
+
+from sqlalchemy import (create_engine)
+from sqlalchemy.orm import sessionmaker
+
+if __name__ == "__main__":
+    myU = argv[1]
+    myP = argv[2]
+    myDB = argv[3]
+    myH = "localhost"
+    db = 'mysql+mysqldb://{}:{}@localhost/{}'.format(myU, myP, myDB)
+    engine = create_engine(db, pool_pre_ping=True)
+    Base.metadata.create_all(engine)
+
+    presession = sessionmaker(bind=engine)
+    session = presession()
+    result = session.query(State).filter(State.name.like('%a%'))
+    result.delete(synchronize_session='fetch')
+    session.commit()
+    session.close()
