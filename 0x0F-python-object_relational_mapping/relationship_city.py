fa13b87656d86428d125c0dd4288bb34072c6f3e
Wiston Venera Macias <2170@holbertonschool.com>
2021-03-26 21:15:10 +0000

    Fixed style

--- 0x0F-python-object_relational_mapping/relationship_city.py
+++ 0x0F-python-object_relational_mapping/relationship_city.py
@@ -4,8 +4,7 @@
 from sqlalchemy import Column, Integer, String, ForeignKey
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import relationship
-Base = declarative_base()
-
+from model_state import Base
 
 class City(Base):
     __tablename__ = 'cities'
