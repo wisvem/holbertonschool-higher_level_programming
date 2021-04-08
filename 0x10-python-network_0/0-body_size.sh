6a765920c84ef38d42ab60d1bead386a49c4a465
Wiston Venera Macias <wisvem@hotmail.com>
2021-04-08 17:33:50 +0000

    Removed carriage return

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/bash
 # displays the size of the body of the response
-curl -sI "$1" | grep Content-Length | cut -d' ' -f2
+curl -sI "$1" | grep Content-Length | cut -d' ' -f2 | tr -d '\r'
