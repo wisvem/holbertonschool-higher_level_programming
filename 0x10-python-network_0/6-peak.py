8574be24864a39ddd80739835d57d3a07a77572c
Wiston Venera Macias <wisvem@hotmail.com>
2021-04-08 22:40:13 +0000

    Task 6

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -4,13 +4,7 @@
 
 def find_peak(list_of_integers):
     """Generic function"""
-    list_len = len(list_of_integers)
-    if (list_len is 0):
+    if not list_of_integers:
         return None
-    x = list_of_integers[0]
-    for i in range(int(list_len/2)):
-        if (x < list_of_integers[0]):
-            x = list_of_integers[0]
-        if (x < list_of_integers[list_len - 1 - i]):
-            x = list_of_integers[list_len - 1 - i]
-    return x
+    list_of_integers.sort()
+    return max(list_of_integers)
