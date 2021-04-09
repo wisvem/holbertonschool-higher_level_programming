266d9c47e138a1a0f21a383cacf062d829bf6024
Wiston Venera Macias <wisvem@hotmail.com>
2021-04-09 18:36:42 +0000

    Task 3

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -5,9 +5,10 @@ from urllib.request import urlopen, Request
 from urllib.parse import urlencode
 from sys import argv
 
-url = argv[1]
-_data = urlencode({'email': argv[2]}).encode('utf-8')
-req = Request(url, data=_data)  # this will make the method "POST"
-with urlopen(req) as r:
-    html = r.read()
-    print('{}'.format(html.decode('utf-8')))
+if __name__ == "__main__":
+    url = argv[1]
+    _data = urlencode({'email': argv[2]}).encode('utf-8')
+    req = Request(url, data=_data)  # this will make the method "POST"
+    with urlopen(req) as r:
+        html = r.read()
+        print('{}'.format(html.decode('utf-8')))
