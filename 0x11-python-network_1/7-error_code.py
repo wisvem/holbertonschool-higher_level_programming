272394fb91a44be759d570f0327d9bc5263c8017
Wiston Venera Macias <wisvem@hotmail.com>
2021-04-09 20:23:15 +0000

    Task 7

---
+++ 0x11-python-network_1/7-error_code.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+from sys import argv
+from requests import post, get, HTTPError
+
+if __name__ == "__main__":
+    r = get(argv[1])
+    if r:
+        print(r.text)
+    else:
+        print("Error code: {}".format(r.status_code))
