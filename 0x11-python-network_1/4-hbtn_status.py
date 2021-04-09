7e82689f4049ed3e1b42497885175c49b91b142a
Wiston Venera Macias <wisvem@hotmail.com>
2021-04-09 19:27:35 +0000

    Task 4 - Added print("Body response:")

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -5,5 +5,6 @@ from requests import get
 
 if __name__ == "__main__":
     r = get('https://intranet.hbtn.io/status')
+    print("Body response:")
     print("\t- type: {}".format(type(r.text)))
     print("\t- content: {}".format(r.text))
