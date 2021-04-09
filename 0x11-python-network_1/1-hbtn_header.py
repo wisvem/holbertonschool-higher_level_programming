02248f42b80747bd96291e5915f3b5e9d95bef6b
Wiston Venera Macias <wisvem@hotmail.com>
2021-04-09 18:22:16 +0000

    Task 1 - removed format line 6

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -3,6 +3,6 @@
 
 from urllib.request import urlopen
 from sys import argv
-with urlopen('{}'.format(argv[1])) as r:
-    html = r.info().get('X-Request-Id')
-    print(html)
+with urlopen(argv[1]) as r:
+    html = r.info()
+    print(html['X-Request-Id'])
