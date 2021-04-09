df5f5c1b2d10bb1e1173d450ad8d3bfc428ed0ea
Wiston Venera Macias <wisvem@hotmail.com>
2021-04-09 19:47:04 +0000

    Task 5 - Changed the way to get the data

--- 0x11-python-network_1/5-hbtn_header.py
+++ 0x11-python-network_1/5-hbtn_header.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 """ 5. Response header value #1 """
 
-from requests import head
+from requests import get
 from sys import argv
 
 if __name__ == "__main__":
-    r = head(argv[1])
-    print(r.headers['X-Request-Id'])
+    r = get(argv[1])
+    print(r.headers.get('X-Request-Id'))
