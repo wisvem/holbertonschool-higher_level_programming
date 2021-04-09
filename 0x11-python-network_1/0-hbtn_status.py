60c8ce9dbd7798faf8463d61df37d674a043eb12
Wiston Venera Macias <wisvem@hotmail.com>
2021-04-09 18:31:08 +0000

    Tasks 0-1 Added if __name__ == __main__:

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -2,9 +2,11 @@
 """ 0. What's my status? #0 """
 
 from urllib.request import urlopen
-with urlopen('https://intranet.hbtn.io/status') as r:
-    html = r.read()
-    print("Body response:")
-    print("\t- type: {}".format(type(html)))
-    print("\t- content: {}".format(html))
-    print("\t- utf8 content: {}".format(html.decode("utf-8")))
+
+if __name__ == "__main__":
+    with urlopen('https://intranet.hbtn.io/status') as r:
+        html = r.read()
+        print("Body response:")
+        print("\t- type: {}".format(type(html)))
+        print("\t- content: {}".format(html))
+        print("\t- utf8 content: {}".format(html.decode("utf-8")))
--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -3,6 +3,7 @@
 
 from urllib.request import urlopen
 from sys import argv
-with urlopen(argv[1]) as r:
-    html = r.headers.get('X-Request-Id')
-    print(html)
+if __name__ == "__main__":
+    with urlopen(argv[1]) as r:
+        html = r.info()
+        print(html['X-Request-Id'])
