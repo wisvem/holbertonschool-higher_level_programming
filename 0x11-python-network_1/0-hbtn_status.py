a689888edd7e931524b6108a27d4171a60a911f8
Wiston Venera Macias <wisvem@hotmail.com>
2021-04-09 18:05:35 +0000

    Task 0 - Changed spaces by tabs

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -5,6 +5,6 @@ from urllib.request import urlopen
 with urlopen('https://intranet.hbtn.io/status') as r:
     html = r.read()
     print("Body response:")
-    print("    - type: {}".format(type(html)))
-    print("    - content: {}".format(html))
-    print("    - utf8 content: {}".format(html.decode("utf-8")))
+    print("\t- type: {}".format(type(html)))
+    print("\t- content: {}".format(html))
+    print("\t- utf8 content: {}".format(html.decode("utf-8")))
