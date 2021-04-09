a3c718dfab6fe5918cc0988b36da4d94dfc55112
Wiston Venera Macias <wisvem@hotmail.com>
2021-04-09 19:41:37 +0000

    Task 5 - Changed get by head

--- 0x11-python-network_1/5-hbtn_header.py
+++ 0x11-python-network_1/5-hbtn_header.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 """ 5. Response header value #1 """
 
-from requests import get
+from requests import head
 from sys import argv
 
 if __name__ == "__main__":
-    r = get(argv[1])
+    r = head(argv[1])
     print(r.headers['X-Request-Id'])
