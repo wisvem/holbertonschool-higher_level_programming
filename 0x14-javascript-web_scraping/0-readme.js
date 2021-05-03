94e22dc595d229fd0f52304fd47425eb2de798a3
Wiston Venera Macías <wisvem@hotmail.com>
2021-05-03 04:37:45 +0000

    Task 1 - Fixed semistandard style

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
-const file = process.argv.slice(2)[0]
-fs = require('fs')
-fs.readFile(file, 'utf8', function (err,data) {
+const file = process.argv.slice(2)[0];
+const fs = require('fs');
+fs.readFile(file, 'utf8', function (err, data) {
   if (err) {
     return console.log(err);
   }
