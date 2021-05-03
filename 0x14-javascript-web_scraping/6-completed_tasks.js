7b25560b395691ec5bd0d03dabf6a0b71a3de0f9
Wiston Venera Macías <wisvem@hotmail.com>
2021-05-03 09:46:42 +0000

    Task 6

---
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -0,0 +1,24 @@
+#!/usr/bin/node
+const request = require('request');
+const url = process.argv[2];
+let count = 0;
+const userTasks = {};
+let user = '';
+request(url, function (error, response, body) {
+  if (error) {
+    return console.log(error);
+  }
+  const result = JSON.parse(body);
+
+  result.forEach(element => {
+    if (user !== element.userId) {
+      count = 0;
+      user = element.userId;
+    }
+    if (element.completed === true) {
+      count++;
+      userTasks[element.userId] = count;
+    }
+  });
+  console.log(userTasks);
+});
