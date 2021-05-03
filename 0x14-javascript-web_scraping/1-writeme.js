631a20962ca6f55f01f18479de0a48acc3f0c60b
Wiston Venera Macías <wisvem@hotmail.com>
2021-05-03 04:50:08 +0000

    Task 1

--- 0x14-javascript-web_scraping/1-writeme.js
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -1,1 +1,9 @@
 #!/usr/bin/node
+const file = process.argv.slice(2)[0];
+const text = process.argv.slice(2)[1];
+const fs = require('fs');
+fs.writeFile(file, text, 'utf8', function (err) {
+  if (err) {
+    return console.log(err);
+  }
+});
