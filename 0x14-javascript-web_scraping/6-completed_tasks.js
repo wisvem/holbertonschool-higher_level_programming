cdb3db227ce54ba658999e26c947e97c25b54e2c
Wiston Venera Macías <wisvem@hotmail.com>
2021-05-03 11:16:25 +0000

    Task 6 - refactoring

--- 0x14-javascript-web_scraping/6-completed_tasks.js
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -1,22 +1,21 @@
 #!/usr/bin/node
 const request = require('request');
 const url = process.argv[2];
-let count = 0;
 const userTasks = {};
-let user = '';
 request(url, function (error, response, body) {
   if (error) {
     return console.log(error);
   }
   const result = JSON.parse(body);
+
   result.forEach(element => {
     if (element.completed === true) {
-      if (user !== element.userId) {
-        count = 0;
-        user = element.userId;
+      if (userTasks[element.userId] === undefined) {
+        userTasks[element.userId] = 1;
+      } else {
+        // add +1 to number of task
+        userTasks[element.userId]++;
       }
-      count = count + 1;
-      userTasks[element.userId] = count;
     }
   });
   console.log(userTasks);
